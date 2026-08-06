88803664012462a124ab4799fb6420f44959fe5e
Deviya Bai <divyaharsinghani05@gmail.com>
2026-08-06 22:34:49 +0500

    Initial commit - Number Guessing Game Java

---
+++ README.md
@@ -0,0 +1,17 @@
+# Number Guessing Game Java
+
+A Java-based Number Guessing Game where the user tries to guess a randomly generated number.
+
+## Features
+
+- Random number generation
+- User input for guessing
+- Higher and lower hints
+- Attempt counting
+- Result display
+
+## Technologies Used
+
+- Java
+
+## Project Structure
---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+
+        int number = random.nextInt(100) + 1;
+        int guess;
+        int attempts = 0;
+
+        System.out.println("===== NUMBER GUESSING GAME =====");
+
+        do {
+
+            System.out.print("Enter your guess: ");
+            guess = input.nextInt();
+
+            attempts++;
+
+            if (guess > number) {
+
+                System.out.println("Try a lower number");
+
+            } else if (guess < number) {
+
+                System.out.println("Try a higher number");
+
+            } else {
+
+                System.out.println("Correct guess!");
+                System.out.println("You guessed the number in " + attempts + " attempts");
+
+            }
+
+        } while (guess != number);
+
+
+        input.close();
+    }
+}
